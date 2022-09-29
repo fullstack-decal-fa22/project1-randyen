e6cc23032aead409d66ea5b642712cc57ea50e4c
Randy Nguyen <randyn@berkeley.edu>
2022-09-28 19:41:33 -0700

    Finished proj spec reqs

---
+++ input.js
@@ -0,0 +1,8 @@
+function check() {
+    const submission = document.getElementById('submission');
+    if (submission.value === "Kaguya Shinomiya") {
+        alert("That's correct!");
+    } else {
+        alert("Sorry, try again.")
+    }
+}
